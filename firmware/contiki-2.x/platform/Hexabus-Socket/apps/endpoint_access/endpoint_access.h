15cc3d57cdb7aefcacf97f68b6958e386028b2e1
Bernd Lietzow <github@ebrnd.de>
2011-11-09 14:08:50 +0100

    fixed function declarations in header

--- firmware/contiki-2.x/platform/Hexabus-Socket/apps/endpoint_access/endpoint_access.h
+++ firmware/contiki-2.x/platform/Hexabus-Socket/apps/endpoint_access/endpoint_access.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 #include "../../../../../../shared/hexabus_packet.h"
 
-uint8_t get_datatype(uint8_t eid);
-uint8_t write(uint8_t eid, struct hxb_value value);
-struct hxb_value read(uint8_t eid);
+uint8_t endpoint_get_datatype(uint8_t eid);
+uint8_t endpoint_write(uint8_t eid, struct hxb_value* value);
+void read(uint8_t eid, struct hxb_value* value);
 
 #endif
